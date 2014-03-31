2b0cd1f0b21c20ace43ea0a3bd477318ea2c68e9
Dmitriy Konovalov <d.konovalov.39@gmail.com>
2014-03-31 16:04:43 +0300

    registration controller fixed

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -5,17 +5,22 @@ class RegistrationController < ApplicationController
   end
 
   def create
-    @user = User.new
-    @user.name = params[:user][:name]
-    @user.password = params[:user][:password]
-    @user.email = params[:user][:email]
+    @user = User.new(user_params)
+    #@user.name = params[:user][:name]
+    #@user.password = params[:user][:password]
+    #@user.email = params[:user][:email]
 
-    if @user.errors.blank?
-      @user.save
+    if @user.save
       redirect_to projects_path
     else
       render 'new'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email)
   end
 
 
