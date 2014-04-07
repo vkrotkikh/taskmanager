fd1a20b1c0e57001ebeb79fe68f59e78be7f88b2
Dmitriy Konovalov <d.konovalov.39@gmail.com>
2014-04-07 16:51:51 +0300

    spec files added

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,10 +2,14 @@ FactoryGirl.define do
   factory :user do
     name {Faker::Name.name}
     email {Faker::Internet.email}
-    password {Faker::Internet.password}
+    password {"user_pass#{Faker::Internet.password}"}
   end
 
   factory :project do
     name {Faker::Company.name}
+
+    factory :invalid_project do
+      name nil
+    end
   end
 end
